2bf0c8aa3675fd00e6f60803eb617bd6a77b5894
amorales <alvaro.gabriel.morales@gmail.com>
2023-05-26 18:03:09 -0300

    Debt control done

--- src/components/pBelInsQuote/DebtControl.jsx
+++ src/components/pBelInsQuote/DebtControl.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useKeycloak } from "@react-keycloak/web";
+
 import { selectOnAuthSuccess } from "../../reduxToolkit/userSlice";
 import { selectPBelFlowSelectedData } from "../../reduxToolkit/pBel/pBelFlowSlice";
 import { clientApi } from "../../utils/clientApi";
