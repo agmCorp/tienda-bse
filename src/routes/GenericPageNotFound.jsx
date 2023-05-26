8824d5e4d7e3ff64bd8db3da50c0be6040a4bedb
amorales <alvaro.gabriel.morales@gmail.com>
2023-05-25 23:38:41 -0300

    Debt control unstable

--- src/routes/GenericPageNotFound.jsx
+++ src/routes/GenericPageNotFound.jsx
@@ -1,5 +1,17 @@
+import pageNotFound from "../images/page-not-found.png";
+
 function GenericPageNotFound() {
-  return <div>PAGE NOT FOUND</div>;
+  return (
+    <div
+      className="flex flex-column text-center fixed top-50 left-50"
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img src={pageNotFound} alt="Página no encontrada" className="w-full" />
+      <span className="text-lg md:text-6xl text-500">PAGE NOT FOUND</span>
+    </div>
+  );
 }
 
 export default GenericPageNotFound;
